c7d06834f9db74e2d3eaebf76489729af23d4305
waka <waka6251@gmail.com>
2025-10-24 00:42:17 +0900

    モンスター一覧を表示

--- data/monsters.js
+++ data/monsters.js
@@ -1,11 +1,11 @@
 window.MONSTERS = [
-  { id:"m01", name:"フレイムリン", attr:"炎", hp:120, st:80, atk:18, def:10, mag:6,  spd:12 },
-  { id:"m02", name:"アクアリス",   attr:"水", hp:110, st:90, atk:12, def:12, mag:16, spd:11 },
-  { id:"m03", name:"ゲイルバード", attr:"風", hp:95,  st:85, atk:14, def:10, mag:14, spd:18 },
-  { id:"m04", name:"ロックホーン", attr:"土", hp:140, st:70, atk:16, def:18, mag:4,  spd:8  },
-  { id:"m05", name:"サンダーキッド", attr:"雷", hp:105, st:100, atk:15, def:10, mag:12, spd:16 },
-  { id:"m06", name:"フロストフォックス", attr:"氷", hp:100, st:95, atk:12, def:12, mag:18, spd:12 },
-  { id:"m07", name:"ルクスキャット", attr:"光", hp:115, st:85, atk:13, def:13, mag:13, spd:13 },
-  { id:"m08", name:"ノクタス",     attr:"闇", hp:110, st:90, atk:17, def:9,  mag:10, spd:14 },
-  { id:"m09", name:"マグマタートル", attr:"炎", hp:150, st:70, atk:16, def:20, mag:6,  spd:6  }
+  { id:"m001", name:"エンレン", attr:"炎", hp:120, st:80, atk:18, def:10, mag:6,  spd:12 },
+  { id:"m002", name:"アクアリス",   attr:"水", hp:110, st:90, atk:12, def:12, mag:16, spd:11 },
+  { id:"m003", name:"ゲイルバード", attr:"風", hp:95,  st:85, atk:14, def:10, mag:14, spd:18 },
+  { id:"m004", name:"ロックホーン", attr:"土", hp:140, st:70, atk:16, def:18, mag:4,  spd:8  },
+  { id:"m005", name:"サンダーキッド", attr:"雷", hp:105, st:100, atk:15, def:10, mag:12, spd:16 },
+  { id:"m006", name:"フロストフォックス", attr:"氷", hp:100, st:95, atk:12, def:12, mag:18, spd:12 },
+  { id:"m007", name:"ルクスキャット", attr:"光", hp:115, st:85, atk:13, def:13, mag:13, spd:13 },
+  { id:"m008", name:"ノクタス",     attr:"闇", hp:110, st:90, atk:17, def:9,  mag:10, spd:14 },
+  { id:"m009", name:"マグマタートル", attr:"炎", hp:150, st:70, atk:16, def:20, mag:6,  spd:6  }
 ];
