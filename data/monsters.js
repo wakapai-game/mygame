fd37b1dfed59a3c23134e26460001516fc2bf45b
waka <waka6251@gmail.com>
2025-10-28 01:08:13 +0900

    m003_カザワイ追加

--- data/monsters.js
+++ data/monsters.js
@@ -1,7 +1,7 @@
 window.MONSTERS = [
   { id:"m001", name:"エンレン", attr:"炎", hp:120, st:80, atk:18, def:10, mag:6,  spd:12 },
   { id:"m002", name:"マカイア",   attr:"水", hp:110, st:90, atk:12, def:12, mag:16, spd:11 },
-  { id:"m003", name:"ゲイルバード", attr:"風", hp:95,  st:85, atk:14, def:10, mag:14, spd:18 },
+  { id:"m003", name:"カザワイ", attr:"風", hp:95,  st:85, atk:14, def:10, mag:14, spd:18 },
   { id:"m004", name:"ロックホーン", attr:"土", hp:140, st:70, atk:16, def:18, mag:4,  spd:8  },
   { id:"m005", name:"サンダーキッド", attr:"雷", hp:105, st:100, atk:15, def:10, mag:12, spd:16 },
   { id:"m006", name:"フロストフォックス", attr:"氷", hp:100, st:95, atk:12, def:12, mag:18, spd:12 },
